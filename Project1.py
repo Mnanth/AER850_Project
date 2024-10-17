9c556b9794e7a441682bc504c168f999629a8af9
Mnanth <mmathanaraj@torontomu.ca>
2024-10-16 20:32:13 -0400

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -6,3 +6,12 @@ Created on Sun Oct 13 20:02:05 2024
 @author: mithu
 """
 
+
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+# Step 1: Data Processing
+csv_file = pd.read_csv ('/Users/mithu/Documents/GitHub/AER850_Project/Project_1_Data.csv')
+
+#step 2: Data Visualization 
