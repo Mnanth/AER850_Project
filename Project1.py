aee99d53e2ea2a350a206f570c37986e47ccf16d
Mnanth <mmathanaraj@torontomu.ca>
2024-10-17 11:05:25 -0400

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -10,8 +10,18 @@ Created on Sun Oct 13 20:02:05 2024
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt 
+import seaborn as sns
 
 # Step 1: Data Processing
-csv_file = pd.read_csv ('/Users/mithu/Documents/GitHub/AER850_Project/Project_1_Data.csv')
+df = pd.read_csv ('/Users/mithu/Documents/GitHub/AER850_Project/Project_1_Data.csv')
 
 #step 2: Data Visualization 
+
+df.hist(bins = 12)
+plt.suptitle('Histograms of Coordinates')
+
+
+
+#step 3:Correlation Analysis
+corr_matrix = df.corr()
+print(corr_matrix)
