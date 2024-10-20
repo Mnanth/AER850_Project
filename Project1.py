358d3de625c0297a6e544fc36901c9600ec681b0
Mnanth <mmathanaraj@torontomu.ca>
2024-10-20 03:03:32 -0400

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -12,15 +12,17 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns
 from sklearn.model_selection import train_test_split, StratifiedShuffleSplit 
-from sklearn.modelselection import cross_val_score 
+from sklearn.model_selection import cross_val_score 
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_absolute_error
+from sklearn.svm import SVR
+
 
 # Step 1: Data Processing
 df = pd.read_csv ('/Users/mithu/Documents/GitHub/AER850_Project/Project_1_Data.csv')
 
-print(df.head())
+
 #step 2: Data Visualization 
 #Load data for plot
 x = df.iloc[:,0] 
@@ -52,15 +54,29 @@ print(corr_matrix)
 #Step 4 Classification Model Development/Engineering
 #Data Splitting 
 
-
-#X_train, X_test, y_train, y_test = train_test_split
+#Stratified Sampling
 df["Coordinates"] = pd.cut(df["Step"], bins = [0,2,4,6, np.inf], labels = [1,2,3,4,])
 my_splitter = StratifiedShuffleSplit(n_splits = 1, test_size = 0.2, random_state = 42)
-
+for train_index, test_index in my_splitter.split(df, df["Coordinates"]):
+    strat_df_train = df.loc[train_index].reset_index(drop = True)
+    strat_df_test = df.loc[test_index].reset_index(drop=True)
+    strat_df_train = strat_df_train.drop(columns=["Coordinates"], axis =1)
+    strat_df_test = strat_df_test.drop(columns=["Coordinates"], axis =1)
 
 #Variable Selection
 #X_columns = 'X', 'Y', 'Z'
 #y_columns = 'Step'
+X_train = strat_df_train.drop("Step", axis = 1)
+y_train = strat_df_train["Step"]
+X_test = strat_df_test.drop("Step", axis = 1)
+y_test = strat_df_test["Step"]
 
-#Regression
+
+
+#Linear Regression
+mymodel_1 = LinearRegression()
+param_grid_lr = {}
+grid_search_lr = GridSearchCV(linear_reg,param_grid_lr, scoring = 'neg_mean_absolute_error')
+
+
 #Logistic
