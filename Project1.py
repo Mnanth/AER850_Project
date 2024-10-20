9db3718c58d5e564f80ad900cae33df381628b30
Mnanth <mmathanaraj@torontomu.ca>
2024-10-20 01:23:44 -0400

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -11,6 +11,11 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt 
 import seaborn as sns
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit 
+from sklearn.modelselection import cross_val_score 
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_absolute_error
 
 # Step 1: Data Processing
 df = pd.read_csv ('/Users/mithu/Documents/GitHub/AER850_Project/Project_1_Data.csv')
@@ -22,6 +27,7 @@ x = df.iloc[:,0]
 y = df.iloc[:,1]
 z = df.iloc[:,2]
 step = df.iloc[:,3]
+
 #plot
 fig=plt.figure()
 ax = fig.add_subplot(projection = '3d') 
@@ -44,5 +50,17 @@ print(corr_matrix)
 
 
 #Step 4 Classification Model Development/Engineering
+#Data Splitting 
+
+
+#X_train, X_test, y_train, y_test = train_test_split
+df["Coordinates"] = pd.cut(df["Step"], bins = [0,2,4,6, np.inf], labels = [1,2,3,4,])
+my_splitter = StratifiedShuffleSplit(n_splits = 1, test_size = 0.2, random_state = 42)
+
+
+#Variable Selection
+#X_columns = 'X', 'Y', 'Z'
+#y_columns = 'Step'
+
 #Regression
 #Logistic
